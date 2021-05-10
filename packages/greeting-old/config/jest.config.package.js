b63668fe888408e908761ff112442e59b1c25e2d
Bing Ren <bingtimren@gmail.com>
2021-05-10 15:53:53 +1000

    remove old packages

--- packages/greeting-old/config/jest.config.package.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  rootDir: "../tests/package",
-  testMatch: ["**/*.test.ts"],
-  testEnvironment: "node",
-};
--- packages/word-old/src/commons.test.ts
+++
@@ -1,7 +0,0 @@
-import * as commons from './commons';
-
-describe('Example Test Suite', () => {
-  it('Example test case', () => {
-    expect(typeof commons.WORD).toEqual('string');
-  });
-});
