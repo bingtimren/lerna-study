05dc32dc4cbabd82937acaa3784e6b133ace1ef5
Bing Ren <bingtimren@gmail.com>
2021-05-12 19:54:41 +1000

    remove two compiled files mixed with source

--- packages/words/src/words.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var words = require("./words");
-describe("@bingsjs/words", function () {
-    it("should export word hi", function () {
-        expect(typeof words.hi).toBe("string");
-    });
-});
