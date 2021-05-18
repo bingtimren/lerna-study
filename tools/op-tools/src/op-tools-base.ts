8df520945e677bfaeb9bfbb9f3a637abb4b07892
Bing Ren <bingtimren@gmail.com>
2021-05-18 16:57:41 +1000

    op-tools complete test done

--- tools/op-tools/src/op-tools-base.ts
+++ tools/op-tools/src/op-tools-base.ts
@@ -9,22 +9,41 @@ import * as chalk from "chalk";
 import * as childProcess from "child_process";
 import { parse as jsoncParse } from "jsonc-parser";
 
+/**
+ * Options for initiating an OpinionedCommand instance
+ */
 interface OpinionedCommandOptions {
   configFilesRelativeDir: string;
   configFileSuffix: string;
   defaultConfig: string;
+  localCopyDir: string;
 }
 
-const defaultOptions: OpinionedCommandOptions = {
+export const defaultOptions: OpinionedCommandOptions = {
   configFileSuffix: ".json",
   configFilesRelativeDir: "pre-configs",
   defaultConfig: "default",
+  localCopyDir: ".opToolsConfig",
 };
 
+/**
+ * OpinionedCommand
+ * This class contains common functionality for implementing "opinioned tools", i.e. tools  (eslint, jest, tsc, etc.) with
+ * pre-configurations and additional packages (plug-ins etc).
+ */
 export class OpinionedCommand {
   private prog: commander.Command;
   private dirOptions: OpinionedCommandOptions;
   private localConfigPath: string | undefined;
+  /**
+   *
+   * @param baseDir the base dir of the "opinioned tools" package, used to resolve pre-configuration files, usually the "__dirname" from the calling script
+   * @param options optional options:
+   *    - configFileSuffix: default to ".json",
+   *    - configFilesRelativeDir: the folder containing the config files, relative to the baseDir, default to "pre-configs",
+   *    - defaultConfig: the base name of the default config file, default to "default",
+   *    - localCopyDir: default to ".opToolsConfig", the relative dir if {@link localCopyConfig} is called
+   */
   constructor(
     private baseDir: string,
     options: Partial<OpinionedCommandOptions> = {}
@@ -49,6 +68,7 @@ export class OpinionedCommand {
           );
         }
         fs.readdirSync(this.configDir)
+          .filter((fname) => fname.endsWith(this.dirOptions.configFileSuffix))
           .map((fname) =>
             fname.substr(
               0,
@@ -58,7 +78,6 @@ export class OpinionedCommand {
           .forEach((conf) => {
             console.log(conf);
           });
-        process.exit(0);
       });
     this.prog
       .command("print")
@@ -67,38 +86,69 @@ export class OpinionedCommand {
         if (this.opts.verbose) {
           console.log(chalk.blueBright("Pre-config: ") + this.opts.preConfig);
           console.log(
-            chalk.blueBright("Pre-config File: ") + this.configFilePath
+            chalk.blueBright("Pre-config File: ") + this.configFilePathInPackage
           );
         }
-        console.log(this.getConfigFileContent().toString());
+        console.log(fs.readFileSync(this.configFilePathInPackage).toString());
       });
   }
 
+  /**
+   * The Commander "program" object, see https://github.com/tj/commander.js/
+   */
   public get program(): commander.Command {
     return this.prog;
   }
+  /**
+   * The dir in the opinioned tools package that contains the pre-configuration files
+   */
   public get configDir(): string {
     return path.join(this.baseDir, this.dirOptions.configFilesRelativeDir);
   }
+  /**
+   * The selected pre-configuration file's path. This is either:
+   * - the relative path of the locally copied configuration file, if {@link localCopyConfig} is called, or
+   * - the absolute path of the pre-configuration file in the opinioned tool package, i.e. baseDir + configFilesRelativeDir + selectedConfig + configFileSuffix
+   */
   public get configFilePath(): string {
     return this.localConfigPath
       ? this.localConfigPath
       : this.configFilePathInPackage;
   }
+  /**
+   * path of the selected pre-configuration file in the opinioned tools package (relative to baseDir)
+   */
   public get configFilePathInPackage(): string {
     return path.join(
       this.configDir,
       `${this.opts.preConfig}${this.dirOptions.configFileSuffix}`
     );
   }
+  /**
+   * if {@link localCopyConfig} is called, path relative to current dir, of the copied configuration file,
+   * otherwise 'undefined'
+   */
   public get configFilePathCopiedLocal(): string | undefined {
     return this.localConfigPath;
   }
+  /**
+   * the options passed to constructor
+   */
+  public get constructOptions(): OpinionedCommandOptions {
+    return this.dirOptions;
+  }
+  /**
+   *
+   * @returns the content of the configuration file as a Buffer object
+   */
   public getConfigFileContent(): Buffer {
     return fs.readFileSync(this.configFilePath);
   }
-  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-  public getConfigFileContentParsed(): any {
+  /**
+   *
+   * @returns the content of the configuration file, parsed to javascript object according to suffix (.json, .jsonc, .js)
+   */
+  public getConfigFileContentParsed(): unknown {
     if (this.configFilePath.endsWith(".jsonc")) {
       return jsoncParse(this.getConfigFileContent().toString());
     } else if (this.configFilePath.endsWith(".json")) {
@@ -111,13 +161,34 @@ export class OpinionedCommand {
     }
     throw new Error(`Cannot parse ${this.configFilePath}, unknown file type.`);
   }
+  /**
+   * the command line options parsed by Commander.js
+   */
   public get opts(): commander.OptionValues {
     return this.prog.opts();
   }
-  public async parse(argv?: string[]): Promise<void> {
-    this.prog.parseAsync(argv ? argv : process.argv);
+  /**
+   * call the parse() method of the Commander.js "program" object,
+   * see https://github.com/tj/commander.js/#parse-and-parseasync
+   * @param argv - the CLI arguments, usually process.argv
+   */
+  public parse(argv: string[]): void {
+    this.prog.parse(argv);
   }
-  public localCopyConfig(fname: string, dirName = ".opToolsConfig"): void {
+  /**
+   * copy the pre-configuration file from opinioned tool package to a dir relative to current dir
+   * @param fname - the local file name, must match the configuration file suffix (default to '.json')
+   * @param dirName - optional, default to "localCopyDir" option, which default to ".opToolsConfig"
+   */
+  public localCopyConfig(
+    fname: string,
+    dirName = this.dirOptions.localCopyDir
+  ): void {
+    if (!fname.endsWith(this.dirOptions.configFileSuffix)) {
+      throw new Error(
+        `local file name ${fname} has different suffix than ${this.dirOptions.configFileSuffix}`
+      );
+    }
     if (!fs.existsSync(dirName)) {
       fs.mkdirSync(dirName);
     }
@@ -125,11 +196,20 @@ export class OpinionedCommand {
       throw new Error(`${dirName} is not a directory`);
     }
     const localConfigPath = path.join(dirName, fname);
-    fs.copyFileSync(this.configFilePathInPackage, localConfigPath);
+    fs.copyFileSync(
+      this.configFilePathInPackage,
+      localConfigPath,
+      fs.constants.COPYFILE_FICLONE
+    );
     this.localConfigPath = localConfigPath;
   }
-  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-  public chalkedExecSync(CMD: string, exitOnError = true): any {
+  /**
+   * executes command, if verbose option is on, outputs commands first
+   * catches error if execution failed, and returns the error object, error.status may contain the exit code of the command
+   * @param CMD
+   * @returns error object if execution fails, or nothing
+   */
+  public chalkedExecSync(CMD: string): unknown {
     if (this.opts.verbose) {
       console.log(chalk.cyanBright("Running command: ") + CMD);
     }
@@ -137,11 +217,7 @@ export class OpinionedCommand {
       childProcess.execSync(CMD, { stdio: "inherit" });
     } catch (err) {
       console.error(chalk.redBright("ERROR: command failed: ") + CMD);
-      if (exitOnError) {
-        process.exit(err.status);
-      } else {
-        return err;
-      }
+      return err;
     }
   }
 }
--- tools/op-tools/test/op-tools.test.ts
+++ tools/op-tools/test/op-tools.test.ts
@@ -1,43 +1,163 @@
-import { OpinionedCommand } from "..";
+import { OpinionedCommand, defaultOptions } from "../src/op-tools-base";
 import { statSync, readFileSync, rmSync } from "fs";
 import { join } from "path";
-describe("op-tools/OpinionedCommand", () => {
-  let opCmd: OpinionedCommand;
-  beforeAll(() => {
-    opCmd = new OpinionedCommand(__dirname);
-  });
-  it("getConfigFileContent() able to get config file content", () => {
-    expect(opCmd.getConfigFileContent()).toBeInstanceOf(Buffer);
-  });
-  it("configDir() able to get config file dir", () => {
-    const dirPath = opCmd.configDir;
-    const stat = statSync(dirPath);
-    expect(stat.isDirectory()).toEqual(true);
-  });
-  it("getConfigFileContentParsed() able to get config object", () => {
-    expect(opCmd.getConfigFileContentParsed().one).toEqual(1);
-    expect(
-      new OpinionedCommand(__dirname, {
-        configFileSuffix: ".js",
-      }).getConfigFileContentParsed().one
-    ).toEqual(1);
-    expect(
-      new OpinionedCommand(__dirname, {
-        configFileSuffix: ".jsonc",
-      }).getConfigFileContentParsed().one
-    ).toEqual(1);
-  });
-  it("localCopyConfig() should copy config file to local", () => {
-    opCmd.localCopyConfig("a.json");
-    expect(opCmd.configFilePathCopiedLocal).toEqual(
-      join(".opToolsConfig", "a.json")
-    );
-    expect(opCmd.configFilePath).toEqual(opCmd.configFilePathCopiedLocal);
-    const expectedLocalPath = join(process.cwd(), ".opToolsConfig/a.json");
-    expect(readFileSync(expectedLocalPath).toString()).toEqual(
-      opCmd.getConfigFileContent().toString()
-    );
-    // clean up
-    rmSync(expectedLocalPath);
-  });
+import { Command } from "commander";
+
+const mockedLog = jest.spyOn(console, "log");
+const mockedError = jest.spyOn(console, "error");
+
+const testTable = [];
+for (const useLocalCopy of [true, false]) {
+  for (const suffix of [".js", ".json", ".jsonc"]) {
+    testTable.push([useLocalCopy, suffix]);
+  }
+}
+
+afterAll(() => {
+  mockedLog.mockRestore();
+  mockedError.mockRestore();
+  rmSync(".opToolsConfig", { recursive: true, force: true });
 });
+
+describe.each(testTable)(
+  "(local-copy:%p suffix:%p) op-tools/OpinionedCommand",
+  (useLocalCopy: boolean, suffix: string) => {
+    let opCmd: OpinionedCommand;
+
+    beforeEach(() => {
+      mockedLog.mockReset();
+      mockedError.mockReset();
+      opCmd =
+        suffix !== defaultOptions.configFileSuffix
+          ? new OpinionedCommand(__dirname, {
+              configFileSuffix: suffix,
+            })
+          : new OpinionedCommand(__dirname);
+      if (useLocalCopy) {
+        opCmd.localCopyConfig("localCopyTest" + suffix);
+      }
+    });
+    it(`getConfigFileContent() able to get config file content`, () => {
+      expect(opCmd.getConfigFileContent()).toBeInstanceOf(Buffer);
+    });
+    it(`getter configDir able to get config file dir`, () => {
+      const dirPath = opCmd.configDir;
+      const stat = statSync(dirPath);
+      expect(stat.isDirectory()).toEqual(true);
+      const fileStat = statSync(join(dirPath, "default.json"));
+      expect(fileStat.isFile()).toEqual(true);
+    });
+    it(`getter configFilePath able to get config file`, () => {
+      expect(statSync(opCmd.configFilePath).isFile()).toEqual(true);
+    });
+    it(`getter configFilePathInPackage able to get config file`, () => {
+      expect(statSync(opCmd.configFilePathInPackage).isFile()).toEqual(true);
+    });
+    it(`getter configFilePathCopiedLocal able to get config file, if already locally copied`, () => {
+      if (useLocalCopy) {
+        expect(statSync(opCmd.configFilePathCopiedLocal).isFile()).toEqual(
+          true
+        );
+      } else {
+        expect(opCmd.configFilePathCopiedLocal).toBeUndefined();
+      }
+    });
+    it(`getter constructOptions able to get the options`, () => {
+      expect(opCmd.constructOptions.configFileSuffix).toEqual(suffix);
+    });
+    it(`getConfigFileContentParsed() able to get config object`, () => {
+      expect((opCmd.getConfigFileContentParsed() as any).one).toEqual(1);
+    });
+    it(`able to handle "list" command with '-v' parameter`, () => {
+      opCmd.parse(["node", "op-tools.js", "-v", "list"]);
+      expect(console.log).toHaveBeenCalledWith("default");
+      expect(console.log).toHaveBeenCalledWith("alternate");
+      expect(
+        mockedLog.mock.calls
+          .map((messages) => messages.join(" "))
+          .join(" ")
+          .match(/Pre-configuration file dir\:/).length
+      ).toEqual(1);
+    });
+    it(`able to handle "list" command`, () => {
+      opCmd.parse(["node", "op-tools.js", "list"]);
+      expect(console.log).toHaveBeenCalledWith("default");
+      expect(console.log).toHaveBeenCalledWith("alternate");
+    });
+    it(`able to handle "print" command`, () => {
+      opCmd.parse(["node", "op-tools.js", "print"]);
+      expect(
+        mockedLog.mock.calls
+          .map((messages) => messages.join(" "))
+          .join(" ")
+          .match(/\"?one\"?\s*\:\s*1\s*,?/).length
+      ).toEqual(1);
+    });
+    it(`able to handle "print" command with -c parameter`, () => {
+      opCmd.parse(["node", "op-tools.js", "-c", "alternate", "print"]);
+      expect(
+        mockedLog.mock.calls
+          .map((messages) => messages.join(" "))
+          .join(" ")
+          .match(/\"?alternateOne\"?\s*\:\s*1\s*,?/).length
+      ).toEqual(1);
+    });
+    it(`able to handle "print" command with -c -v parameter`, () => {
+      opCmd.parse(["node", "op-tools.js", "-c", "alternate", "-v", "print"]);
+      expect(
+        mockedLog.mock.calls
+          .map((messages) => messages.join(" "))
+          .join(" ")
+          .match(/\"?alternateOne\"?\s*\:\s*1\s*,?/).length
+      ).toEqual(1);
+      expect(
+        mockedLog.mock.calls
+          .map((messages) => messages.join(" "))
+          .join(" ")
+          .match(/Pre\-config\: .*alternate.*Pre-config File\:.*alternate/)
+          .length
+      ).toEqual(1);
+    });
+    it(`able to access program from getter`, () => {
+      expect(opCmd.program).toBeInstanceOf(Command);
+    });
+    it(`throws if config file of unknown type`, () => {
+      const opCmdIllegal = new OpinionedCommand(__dirname, {
+        configFileSuffix: ".unknown",
+      });
+      expect(() => {
+        opCmdIllegal.getConfigFileContentParsed();
+      }).toThrowError(/unknown file type/);
+    });
+    it(`throws if copy local is provided with an inconsistent suffix`, () => {
+      expect(() => {
+        opCmd.localCopyConfig("file.unknown");
+      }).toThrowError(/has different suffix/);
+    });
+    it(`throws if copy local is provided with an invalid dir`, () => {
+      expect(() => {
+        opCmd.localCopyConfig("file" + suffix, "package.json");
+      }).toThrowError(/is not a directory/);
+    });
+    it(`chalkedExecSync runs a command should return undefined`, () => {
+      opCmd.parse(["node", "op-tools.js", "-v", "list"]);
+      expect(opCmd.chalkedExecSync("ls")).toEqual(undefined);
+      expect(
+        mockedLog.mock.calls
+          .map((messages) => messages.join(" "))
+          .join(" ")
+          .match(/Running command: .*ls/).length
+      ).toEqual(1);
+    });
+    it(`chalkedExecSync runs a failing command should return error`, () => {
+      opCmd.parse(["node", "op-tools.js", "list"]);
+      expect(opCmd.chalkedExecSync("not-a-command-absolutely")).toBeDefined();
+      expect(
+        mockedError.mock.calls
+          .map((messages) => messages.join(" "))
+          .join(" ")
+          .match(/ERROR: command failed: .*not-a-command/).length
+      ).toEqual(1);
+    });
+  }
+);
