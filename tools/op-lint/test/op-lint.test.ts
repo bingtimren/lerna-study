67025f7528c31d98aa77103b359fa40e254e8f0f
Bing Ren <bingtimren@gmail.com>
2021-05-20 23:18:34 +1000

    tested op-lint fix

--- tools/op-lint/test/op-lint.test.ts
+++ tools/op-lint/test/op-lint.test.ts
@@ -1,56 +1,62 @@
 import { execSync } from "child_process";
-
+import { readFileSync, copyFileSync } from "fs";
 describe("op-lint", () => {
-    it("op-lint lints Typescript", () => {
-        try {
-            execSync("yarn op-lint test/dirty-code.ts 2>&1");
-            fail("should throw");
-        } catch (error) {
-            // expect to fail
-            expect(error.stdout.toString()).toMatch(
-                /error\s*Unexpected var\, use let or const instead/
-            );
-            expect(error.stdout.toString()).toMatch(
-                /error\s*Insert \`\;\`\s*prettier\/prettier/
-            );
-        }
-    });
-    it("op-lint lints Javascript", () => {
-        try {
-            execSync("yarn op-lint test/dirty-code.js 2>&1");
-            fail("should throw");
-        } catch (error) {
-            // expect to fail
-            expect(error.stdout.toString()).toMatch(
-                /error\s*Unexpected var\, use let or const instead/
-            );
-            expect(error.stdout.toString()).toMatch(
-                /error\s+Insert \`\;\`\s+prettier\/prettier/
-            );
-            expect(error.stdout.toString()).toMatch(
-                /error\s+The object literal notation \{\} is preferrable\s+no\-new\-object/
-            );
-            expect(error.stdout.toString()).toMatch(
-                /error\s+Expected property shorthand\s+object-shorthand/
-            );
-        }
-    });
-    it("op-lint lints JSON", () => {
-        try {
-            execSync("yarn op-lint test/dirty-code.json 2>&1");
-            fail("should throw");
-        } catch (error) {
-            // expect to fail
-            expect(error.stdout.toString()).toMatch(
-                /error\s*Duplicate object key\s+json/
-            );
-            expect(error.stdout.toString()).toMatch(
-                /error\s+Expected comma\s+json/
-            );
-            expect(error.stdout.toString()).toMatch(
-                /error\s+Trailing comma\s+json/
-            );
-        }
-    });
-}
-);
+  it("op-lint lints Typescript", () => {
+    try {
+      execSync("yarn op-lint test/dirty-code.ts 2>&1");
+      fail("should throw");
+    } catch (error) {
+      // expect to fail
+      expect(error.stdout.toString()).toMatch(
+        /error\s*Unexpected var\, use let or const instead/
+      );
+      expect(error.stdout.toString()).toMatch(
+        /error\s*Insert \`\;\`\s*prettier\/prettier/
+      );
+    }
+  });
+  it("op-lint lints Javascript", () => {
+    try {
+      execSync("yarn op-lint test/dirty-code.js 2>&1");
+      fail("should throw");
+    } catch (error) {
+      // expect to fail
+      expect(error.stdout.toString()).toMatch(
+        /error\s*Unexpected var\, use let or const instead/
+      );
+      expect(error.stdout.toString()).toMatch(
+        /error\s+Insert \`\;\`\s+prettier\/prettier/
+      );
+      expect(error.stdout.toString()).toMatch(
+        /error\s+The object literal notation \{\} is preferrable\s+no\-new\-object/
+      );
+      expect(error.stdout.toString()).toMatch(
+        /error\s+Expected property shorthand\s+object-shorthand/
+      );
+    }
+  });
+  it("op-lint lints JSON", () => {
+    try {
+      execSync("yarn op-lint test/dirty-code.json 2>&1");
+      fail("should throw");
+    } catch (error) {
+      // expect to fail
+      expect(error.stdout.toString()).toMatch(
+        /error\s*Duplicate object key\s+json/
+      );
+      expect(error.stdout.toString()).toMatch(/error\s+Expected comma\s+json/);
+      expect(error.stdout.toString()).toMatch(/error\s+Trailing comma\s+json/);
+    }
+  });
+  it("op-lint fixes Typescript and Javascript", () => {
+    execSync("yarn op-lint fix test/fixme.?s 2>&1");
+    const js = readFileSync("test/fixme.js").toString();
+    expect(js).toMatch(/const someValue \= 1\;/);
+    expect(js).toMatch(/someValue\,/);
+    const ts = readFileSync("test/fixme.ts").toString();
+    expect(ts).toMatch(/const someValue \= 1\;/);
+    expect(ts).toMatch(/export const value \= someValue\;/);
+    copyFileSync("test/fixme.ts.backup", "test/fixme.ts");
+    copyFileSync("test/fixme.js.backup", "test/fixme.js");
+  });
+});
