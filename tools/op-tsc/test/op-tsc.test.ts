5ef5699dff541f19b97d64e26d9a27c07805fe2c
Bing Ren <bingtimren@gmail.com>
2021-05-21 08:56:35 +1000

    fix cleanUp for op-tsc test

--- tools/op-tsc/test/op-tsc.test.ts
+++ tools/op-tsc/test/op-tsc.test.ts
@@ -1,21 +1,21 @@
-import { execSync } from "child_process"
-import { rmSync, statSync } from "fs"
-import { join } from "path"
+import { execSync } from "child_process";
+import { rmSync, statSync } from "fs";
+import { join } from "path";
 
 function cleanUp() {
-    const testDist = join(__dirname, "dist");
-    rmSync(testDist, { force: true, recursive: true })
+  rmSync(join(__dirname, "dist"), { force: true, recursive: true });
+  rmSync(join(__dirname, "op-tsc.tsbuildinfo"), { force: true });
 }
 
 describe("op-tsc", () => {
-    it("op-tsc should compile", () => {
-        cleanUp();
-        process.chdir("test");
-        execSync("../dist/op-tsc.js");
-        expect(statSync("dist").isDirectory()).toEqual(true);
-        expect(statSync("dist/good.js").isFile()).toEqual(true);
-        expect(statSync("dist/good.d.ts").isFile()).toEqual(true);
-        expect(statSync("dist/good.d.ts.map").isFile()).toEqual(true);
-        cleanUp();
-    })
-})
+  it("op-tsc should compile", () => {
+    cleanUp();
+    process.chdir("test");
+    execSync("../dist/op-tsc.js");
+    expect(statSync("dist").isDirectory()).toEqual(true);
+    expect(statSync("dist/good.js").isFile()).toEqual(true);
+    expect(statSync("dist/good.d.ts").isFile()).toEqual(true);
+    expect(statSync("dist/good.d.ts.map").isFile()).toEqual(true);
+    cleanUp();
+  });
+});
