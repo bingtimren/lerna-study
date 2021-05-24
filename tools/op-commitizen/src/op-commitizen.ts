75bc7377c32bb2fb771baa532d0e82f2b1d7221f
Bing Ren <bingtimren@gmail.com>
2021-05-24 11:43:11 +1000

    feat(op-commitizen): added --hook option

--- tools/op-commitizen/src/op-commitizen.ts
+++ tools/op-commitizen/src/op-commitizen.ts
@@ -5,14 +5,18 @@ import { OpinionedCommand } from "@bingsjs/op-tools";
 const bootstrap = require('commitizen/dist/cli/git-cz').bootstrap; // eslint-disable-line
 
 const opCmd = new OpinionedCommand(join(__dirname, ".."));
+opCmd.program.option("--hook", "run as git prepare-commit-msg");
 opCmd.program
   .command("commit", { isDefault: true })
   .description("commit with Commitizen")
   .action(() => {
-    bootstrap({
-      cliPath: dirname(require.resolve("commitizen/package.json")),
-      config: opCmd.getConfigFileContentParsed(),
-    });
+    bootstrap(
+      {
+        cliPath: dirname(require.resolve("commitizen/package.json")),
+        config: opCmd.getConfigFileContentParsed(),
+      },
+      opCmd.opts.hook ? ["--hook"] : undefined
+    );
   });
 
 opCmd.parse(process.argv);
