26c587321d0241b349e2ef220c3e3d00644b12ea
Bing Ren <bingtimren@gmail.com>
2021-05-24 12:37:18 +1000

    fix: fix hook command

--- tools/op-commitizen/src/op-commitizen.ts
+++ tools/op-commitizen/src/op-commitizen.ts
@@ -5,17 +5,25 @@ import { OpinionedCommand } from "@bingsjs/op-tools";
 const bootstrap = require('commitizen/dist/cli/git-cz').bootstrap; // eslint-disable-line
 
 const opCmd = new OpinionedCommand(join(__dirname, ".."));
-opCmd.program.option("--hook", "run as git prepare-commit-msg");
 opCmd.program
   .command("commit", { isDefault: true })
   .description("commit with Commitizen")
   .action(() => {
+    bootstrap({
+      cliPath: dirname(require.resolve("commitizen/package.json")),
+      config: opCmd.getConfigFileContentParsed(),
+    });
+  });
+opCmd.program
+  .command("hook <p1> <p2> <p3>")
+  .description("run as git prepare-commit-msg hook")
+  .action((p1: string, p2: string, p3: string) => {
     bootstrap(
       {
         cliPath: dirname(require.resolve("commitizen/package.json")),
         config: opCmd.getConfigFileContentParsed(),
       },
-      opCmd.opts.hook ? ["--hook"] : undefined
+      ["git", "cz", "--hook", p1, p2, p3]
     );
   });
 
