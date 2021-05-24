b4ee9027b687a2e1db35fd20d61b2b6f15be8db5
Bing Ren <bingtimren@gmail.com>
2021-05-24 21:47:01 +1000

    feat(op-lint-staged): added @bingsjs/op-lint-staged

---
+++ tools/op-lint-staged/README.md
@@ -0,0 +1,11 @@
+# `@bingsjs/op-lint-staged`
+
+> TODO: description
+
+## Usage
+
+```
+const opLintStaged = require('@bingsjs/op-lint-staged');
+
+// TODO: DEMONSTRATE API
+```
---
+++ tools/op-lint-staged/src/op-lint-staged.ts
@@ -0,0 +1,27 @@
+import { OpinionedCommand } from "@bingsjs/op-tools";
+import { join } from "path";
+const lintStaged = require('lint-staged'); // eslint-disable-line
+
+const opCmd = new OpinionedCommand(join(__dirname, ".."));
+opCmd.program
+  .command("run", { isDefault: true })
+  .description("(default command) run lint-staged")
+  .action(async () => {
+    const success = await lintStaged({
+      allowEmpty: false,
+      concurrent: true,
+      configPath: opCmd.getConfigFileContentParsed(),
+      cwd: process.cwd(),
+      debug: false,
+      maxArgLength: null,
+      quiet: false,
+      relative: false,
+      shell: false,
+      stash: true,
+      verbose: false,
+    });
+    if (!success) {
+      console.error("lint-staged failed");
+      process.exit(1);
+    }
+  });
