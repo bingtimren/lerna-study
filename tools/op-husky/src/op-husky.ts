acc52e815a41c283d6c8481a1112b38e14e56cc8
Bing Ren <bingtimren@gmail.com>
2021-05-28 14:29:59 +1000

    fix(op-husky): add 'await' so commands are executed sequentially

--- tools/op-husky/src/op-husky.ts
+++ tools/op-husky/src/op-husky.ts
@@ -13,23 +13,27 @@ const opCmd = new OpinionedCommand(join(__dirname, ".."));
 opCmd.program
   .command("install", { isDefault: true })
   .description("(default command) install husky and the git hooks")
-  .action(() => {
+  .action(async () => {
     console.log(chalk.blueBright("Installing husky"));
-    opCmd.chalkedForkPackageBin("husky", undefined, ["install", HUSKYDIR]);
+    await opCmd.chalkedForkPackageBin("husky", undefined, [
+      "install",
+      HUSKYDIR,
+    ]);
     const configSetting: configs =
       opCmd.getConfigFileContentParsed() as configs;
-    Object.entries(configSetting).forEach(([hook, actions]) => {
-      (typeof actions === "string" ? [actions] : actions).forEach((action) => {
+    for (const [hook, actions] of Object.entries(configSetting)) {
+      for (const action of typeof actions === "string" ? [actions] : actions) {
         console.log(
-          chalk.blueBright(`Adding git hook [${hook}] action: `) + action
+          (await chalk.blueBright(`Adding git hook [${hook}] action: `)) +
+            action
         );
         opCmd.chalkedForkPackageBin("husky", undefined, [
           "add",
           join(HUSKYDIR, hook),
           action,
         ]);
-      });
-    });
+      }
+    }
   });
 
 opCmd.parse(process.argv);
