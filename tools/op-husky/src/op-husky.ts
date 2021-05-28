7edba48a94117077824a8bc017350c91968eee8a
Bing Ren <bingtimren@gmail.com>
2021-05-28 11:47:01 +1000

    fix(op-husky): implemented op-husky, needs test

--- tools/op-husky/src/op-husky.ts
+++ tools/op-husky/src/op-husky.ts
@@ -1,7 +1,35 @@
+#!/usr/bin/env node
 import { OpinionedCommand } from "@bingsjs/op-tools";
 import { join } from "path";
+import * as chalk from "chalk";
+
+interface configs {
+  [hookName: string]: string | string[];
+}
+
+const HUSKYDIR = ".husky";
 
 const opCmd = new OpinionedCommand(join(__dirname, ".."));
-opCmd.install;
+opCmd.program
+  .command("install", { isDefault: true })
+  .description("(default command) install husky and the git hooks")
+  .action(() => {
+    console.log(chalk.blueBright("Installing husky"));
+    opCmd.chalkedForkPackageBin("husky", undefined, ["install", HUSKYDIR]);
+    const configSetting: configs =
+      opCmd.getConfigFileContentParsed() as configs;
+    Object.entries(configSetting).forEach(([hook, actions]) => {
+      (typeof actions === "string" ? [actions] : actions).forEach((action) => {
+        console.log(
+          chalk.blueBright(`Adding git hook [${hook}] action: `) + action
+        );
+        opCmd.chalkedForkPackageBin("husky", undefined, [
+          "add",
+          join(HUSKYDIR, hook),
+          action,
+        ]);
+      });
+    });
+  });
 
 opCmd.parse(process.argv);
