a59037df1ed426b4cdce0ec58b60a0f920d44e44
Bing Ren <bingtimren@gmail.com>
2021-05-31 13:17:47 +1000

    refactor(op-tscdoc): change invocation of tsc from using API to fork, prepare for typedoc

--- tools/op-tscdoc/src/op-tscdoc.ts
+++ tools/op-tscdoc/src/op-tscdoc.ts
@@ -2,20 +2,26 @@
 
 import { OpinionedCommand } from "@bingsjs/op-tools";
 import { join } from "path";
-import { build } from "@bingsjs/tsc-prog";
 import { cleanAllFiles } from "ts-purify";
-import { yellowBright, redBright } from "chalk";
+import { yellowBright } from "chalk";
 
 const opCmd = new OpinionedCommand(join(__dirname, ".."), {
-  configFileSuffix: ".tsconfig.jsonc",
+  configFileSuffix: ".tsconfig.json",
 });
 
 opCmd.program
   .command("build", { isDefault: true })
   .description("run tsc --build")
   .action(async () => {
-    const config = opCmd.getConfigFileContentParsed() as any; // eslint-disable-line @typescript-eslint/no-explicit-any
-    // first do purify
+    opCmd.localCopyConfig("op-tscdoc.tsconfig.json");
+
+    // first do purify - remove extraneous .js from outDir that has no corresponding .ts in rootDir
+    const config = opCmd.getConfigFileContentParsed() as {
+      compilerOptions?: {
+        rootDir?: string;
+        outDir?: string;
+      };
+    };
     try {
       if (config?.compilerOptions?.rootDir && config?.compilerOptions?.outDir) {
         await cleanAllFiles(
@@ -31,16 +37,13 @@ opCmd.program
       );
     }
 
-    const buildConfig = {
-      basePath: process.cwd(),
-      compilerOptions: config?.compilerOptions,
-      include: config?.include,
-      exclude: config?.exclude,
-    };
-    const diagnostics = build(buildConfig);
-    if (diagnostics.length > 0) {
-      console.log(redBright("ERROR: tsc build failed"));
-      process.exit(1);
-    }
+    // do tsc --build
+
+    opCmd.chalkedForkPackageBin(
+      "typescript",
+      "tsc",
+      ["--build", opCmd.configFilePathCopiedLocal!],
+      true
+    );
   });
 opCmd.parse(process.argv);
