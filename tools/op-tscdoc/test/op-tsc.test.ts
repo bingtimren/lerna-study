eaee0ca1658ce2e43b8c9592c6849417aec4deb9
Bing Ren <bingtimren@gmail.com>
2021-05-31 14:59:07 +1000

    test(op-tscdoc): tested for doc command

--- tools/op-tscdoc/test/op-tsc.test.ts
+++ tools/op-tscdoc/test/op-tsc.test.ts
@@ -4,18 +4,29 @@ import { join } from "path";
 
 function cleanUp() {
   rmSync(join(__dirname, "dist"), { force: true, recursive: true });
-  rmSync(join(__dirname, "op-tscdoc.tsbuildinfo"), { force: true });
+  rmSync(join(__dirname, "docs"), { force: true, recursive: true });
 }
 
 describe("op-tscdoc", () => {
-  it("op-tscdoc should compile", () => {
+  beforeEach(() => {
     cleanUp();
-    process.chdir("test");
+    process.chdir(__dirname);
+  });
+  afterEach(() => {
+    cleanUp();
+  });
+  it("op-tscdoc should compile", () => {
     execSync("../dist/op-tscdoc.js");
     expect(statSync("dist").isDirectory()).toEqual(true);
     expect(statSync("dist/good.js").isFile()).toEqual(true);
     expect(statSync("dist/good.d.ts").isFile()).toEqual(true);
     expect(statSync("dist/good.d.ts.map").isFile()).toEqual(true);
-    cleanUp();
+  });
+
+  it("op-tscdoc doc should build docs", () => {
+    execSync("../dist/op-tscdoc.js doc");
+    expect(statSync("docs").isDirectory()).toEqual(true);
+    expect(statSync("docs/api/index.html").isFile()).toEqual(true);
+    expect(statSync("docs/api/typedoc.json").isFile()).toEqual(true);
   });
 });
