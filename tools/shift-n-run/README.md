666aeee28afa74e6251e9883095a7ce630ace23c
Bing Ren <bingtimren@gmail.com>
2021-06-04 11:09:56 +1000

    feat(shift-n-run): initial commit

---
+++ tools/shift-n-run/README.md
@@ -0,0 +1,11 @@
+# `shift-n-run`
+
+> TODO: description
+
+## Usage
+
+```
+const shiftNRun = require('shift-n-run');
+
+// TODO: DEMONSTRATE API
+```
---
+++ tools/shift-n-run/jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  // ts-jest
+  preset: "ts-jest",
+  testEnvironment: "node",
+  // paths
+  rootDir: ".",
+  testMatch: ["**/tests/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+  coverageDirectory: "dist/docs/coverage",
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: -10,
+    },
+  },
+  collectCoverageFrom: [
+    "**/src/**/*.ts",
+    "**/src/**/*.js",
+    "!**/node_modules/**",
+  ],
+};
