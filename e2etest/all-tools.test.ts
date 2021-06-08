666aa8286e7fb0a1518147233afa0c44a3811044
Bing Ren <bingtimren@gmail.com>
2021-06-09 01:05:10 +1000

    feat: e2e

--- e2etest/all-tools.test.ts
+++ e2etest/all-tools.test.ts
@@ -1,28 +1,36 @@
 import * as fs from "fs";
 import * as path from "path";
-const nixt = require("nixt");
+import * as execa from "execa";
+import * as pt from "child-process-toolbox";
+
+jest.setTimeout(60000);
 
 beforeAll(async () => {
   process.chdir(path.join(__dirname, "testProj"));
   fs.rmSync(".git", { recursive: true, force: true });
-  nixt()
-    .run("git init")
-    .stdout(/Initialised empty Git repository/)
-    .code(0)
-    .end();
-  nixt().run("git add .").code(0).end();
-  nixt()
-    .run(`git commit -m "init"`)
-    .stdout(/2 files changed/)
-    .code(0)
-    .end();
-  nixt({ colors: false })
-    .run("yarn install")
-    .stdout(/Resolving packages/)
-    .stdout(/success Saved lockfile/)
-    .exist("node_modules")
-    .exist(".husky")
-    .code(0)
-    .end();
+  fs.rmSync(".husky", { recursive: true, force: true });
+  fs.rmSync("node_modules", { recursive: true, force: true });
+  fs.rmSync("yarn.lock", { recursive: true, force: true });
+
+  const gitInit = execa.command("git init");
+  await pt.promiseOutputPattern(gitInit, /Initialised empty Git repository/, {
+    timeoutInMs: 5000,
+  });
+  await gitInit;
+  expect(fs.statSync(".git").isDirectory()).toEqual(true);
+
+  await execa.command("git add .");
+
+  const gitCommit = execa.command(`git commit -m "init"`);
+  await pt.promiseOutputPattern(gitCommit, /3 files changed/, {
+    timeoutInMs: 5000,
+  });
+  await gitCommit;
+
+  await execa.command("yarn install", { stdio: "inherit" });
+  expect(fs.statSync("node_modules").isDirectory()).toEqual(true);
 });
-test("op-husky and git hooks", () => {});
+
+test("op-husky and git hooks", () => {
+  expect(fs.statSync(".husky").isDirectory()).toEqual(true);
+});
