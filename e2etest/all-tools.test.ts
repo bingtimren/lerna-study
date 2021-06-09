a13b4cf83021341548e9ba21fb3e3968a2ce4528
Bing Ren <bingtimren@gmail.com>
2021-06-09 11:52:35 +1000

    test(e2e): fix prepare script

--- e2etest/all-tools.test.ts
+++ e2etest/all-tools.test.ts
@@ -5,32 +5,18 @@ import * as pt from "child-process-toolbox";
 
 jest.setTimeout(60000);
 
-beforeAll(async () => {
+beforeEach(async () => {
+  // clean up but keep husky, yarn.lock and node_modules to save time
   process.chdir(path.join(__dirname, "testProj"));
-  fs.rmSync(".git", { recursive: true, force: true });
-  fs.rmSync(".husky", { recursive: true, force: true });
-  fs.rmSync("node_modules", { recursive: true, force: true });
-  fs.rmSync("yarn.lock", { recursive: true, force: true });
-
-  const gitInit = execa.command("git init");
-  await pt.promiseOutputPattern(gitInit, /Initialised empty Git repository/, {
-    timeoutInMs: 5000,
-  });
-  await gitInit;
-  expect(fs.statSync(".git").isDirectory()).toEqual(true);
-
-  await execa.command("git add .");
-
-  const gitCommit = execa.command(`git commit -m "init"`);
-  await pt.promiseOutputPattern(gitCommit, /3 files changed/, {
-    timeoutInMs: 5000,
-  });
-  await gitCommit;
-
-  await execa.command("yarn install", { stdio: "inherit" });
-  expect(fs.statSync("node_modules").isDirectory()).toEqual(true);
+  execa.commandSync("git reset --hard");
+  console.error(process.cwd());
+  execa.commandSync("git clean -fd");
+  console.error(process.cwd());
+  execa.commandSync("cp -r ../testProjChange/* .", { shell: true });
 });
 
 test("op-husky and git hooks", () => {
   expect(fs.statSync(".husky").isDirectory()).toEqual(true);
 });
+
+test("op-husky and git hooks - commitizen", () => {});
