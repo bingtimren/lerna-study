c5e787f7213f80c021f90367640f67171d58baaf
Bing Ren <bingtimren@gmail.com>
2021-06-09 12:28:39 +1000

    test(e2e): prepare test env

--- e2etest/all-tools.test.ts
+++ e2etest/all-tools.test.ts
@@ -8,10 +8,9 @@ jest.setTimeout(60000);
 beforeEach(async () => {
   // clean up but keep husky, yarn.lock and node_modules to save time
   process.chdir(path.join(__dirname, "testProj"));
+  expect(fs.statSync(".git").isDirectory()).toEqual(true);
   execa.commandSync("git reset --hard");
-  console.error(process.cwd());
   execa.commandSync("git clean -fd");
-  console.error(process.cwd());
   execa.commandSync("cp -r ../testProjChange/* .", { shell: true });
 });
 
@@ -19,4 +18,20 @@ test("op-husky and git hooks", () => {
   expect(fs.statSync(".husky").isDirectory()).toEqual(true);
 });
 
-test("op-husky and git hooks - commitizen", () => {});
+test("op-husky and git hooks - lint and commitizen", async () => {
+  const gitCommit = execa.command("git commit -a");
+  await Promise.all([
+    pt.promiseOutputPattern(gitCommit, /BUILD SCRIPT EXECUTED/, {
+      timeoutInMs: 10000,
+    }),
+    pt.promiseOutputPattern(gitCommit, /TEST SCRIPT EXECUTED/, {
+      timeoutInMs: 10000,
+    }),
+    pt.promiseOutputPattern(
+      gitCommit,
+      /Select the type of change that you're committing/,
+      { timeoutInMs: 10000 }
+    ),
+  ]);
+  await pt.promiseKilled(gitCommit, "SIGKILL");
+});
